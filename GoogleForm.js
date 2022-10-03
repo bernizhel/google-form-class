b836fd45e9fb6a5e19c983661912ee72f5ebd67a
bernizhel <detrop.1995@gmail.com>
2022-10-03 16:43:02 +0300

    refactor,feat: handle radio buttons, modify and validate the options object

--- GoogleForm.js
+++ GoogleForm.js
@@ -1,27 +1,79 @@
+function toTitleCase() {
+  let titledString = '';
+  for (const word of this.split()) {
+    titledString += word.charAt(0)
+      .toUpperCase() + word.substring(1)
+      .toLowerCase();
+  }
+  return titledString;
+}
+
+
+String.prototype.toTitleCase = toTitleCase;
+
+
 class GoogleForm {
   #html = '';
   #options = {};
+  #defaultRequiredError = 'Please, enter this field.';
+  #defaultInvalidError = 'Please, input correct data.';
 
   constructor(options) {
+    // check the options argument for fields to exist
+    if (!options?.title || !options?.fields?.reduce(
+      (acc, nextField) => acc * !!nextField?.name *
+        !!nextField?.type?.keyword,
+      true,
+    )) {
+      throw new TypeError('The options object must contain the "title" and' +
+        ' "fields" keys, and its "fields" key must be an array of object' +
+        ' with keys "name" and "type".');
+    }
     // generate html code for the form instance
     this.#html += '<form>';
-    this.#html += `<h3>${options.title}</h3>`;
     this.#html += '<fieldset>';
-    this.#html += `<legend>${options.description}</legend>`;
+    this.#html += `<legend>${options.title}</legend>`;
+    if (options?.description) {
+      this.#html += `<p>${options.description}</p>`;
+    }
     for (const fieldOptions of options.fields) {
-      this.#html += `<label>${fieldOptions.name}`;
-      this.#html += `<span>${fieldOptions.isRequired ? '*' : ''}</span>`;
-      this.#html += '<br />';
-      this.#html += `<${fieldOptions.tag}`;
-      for (const [attributeKey, attributeValue] of
-        Object.entries(fieldOptions.attributes)) {
-        this.#html += ` ${attributeKey}="${attributeValue}"`;
+      if (fieldOptions.type.keyword === 'radio') {
+        this.#html += '<div>';
+        this.#html += `<span>${fieldOptions.name.toTitleCase()}`;
+        this.#html += `<span>${fieldOptions?.isRequired ? '*' : ''}</span>`;
+        this.#html += '</span>';
+        this.#html += '<br />';
+        this.#html += '<div>';
+        for (const value of fieldOptions.type.values) {
+          this.#html += '<label>';
+          this.#html += `<input type="radio"`;
+          for (const [attributeKey, attributeValue] of
+            Object.entries(fieldOptions.attributes || [])) {
+            this.#html += ` ${attributeKey}="${attributeValue}"`;
+          }
+          this.#html += ` name="${fieldOptions.name}" value="${value}" />`;
+          this.#html += `${value.toTitleCase()}</label>`;
+          this.#html += '<br />';
+        }
+        this.#html += '</div>';
+        this.#html += `<span></span>`;
+        this.#html += '<br />';
+        this.#html += '</div>';
+      } else {
+        this.#html += `<label>${fieldOptions.name.toTitleCase()}`;
+        this.#html += `<span>${fieldOptions?.isRequired ? '*' : ''}</span>`;
+        this.#html += '<br />';
+        this.#html += `<input`;
+        for (const [attributeKey, attributeValue] of
+          Object.entries(fieldOptions.attributes || [])) {
+          this.#html += ` ${attributeKey}="${attributeValue}"`;
+        }
+        this.#html += ' />';
+        this.#html += '<br />';
+        this.#html += `<span></span>`;
+        this.#html += '<br />';
+        this.#html += '</label>';
       }
-      this.#html += ' />';
-      this.#html += '<br />';
-      this.#html += `<span></span>`;
-      this.#html += '</label>';
-      this.#html += '<br />';
     }
     this.#html += '<button type="submit">Submit</button>';
     this.#html += '</fieldset>';
@@ -39,8 +91,13 @@ class GoogleForm {
         event.stopImmediatePropagation();
         if (event.code === 'Enter') {
           event.preventDefault();
-          formElement.querySelector('button[type="submit"]')
-            .focus();
+          if (['radio', 'checkbox'].includes(
+            fieldElement.getAttribute('type'))) {
+            fieldElement.checked = !fieldElement.checked;
+          } else {
+            formElement.querySelector('button[type="submit"]')
+              .focus();
+          }
         }
       });
     }
@@ -49,28 +106,53 @@ class GoogleForm {
         event.stopImmediatePropagation();
         event.preventDefault();
         let willContinueSubmit = true;
-        for (const fieldElement of formElement.querySelectorAll('input')) {
+        for (const fieldElement of
+          formElement.querySelectorAll(
+            'input:not([type="radio"]):not([type="checkbox"])')) {
           const fieldLabelElement = fieldElement.parentElement;
           const fieldOptions = this.#options.fields.find(
             ({name}) => fieldLabelElement.childNodes.item(0).textContent ===
-              name);
+              name.toTitleCase());
           const errorElement = fieldLabelElement.querySelector(
             'span:last-of-type');
           if ((
-            fieldOptions.isRequired || fieldElement.value !== ''
+            fieldOptions?.isRequired || fieldElement.value !== ''
           ) && !fieldOptions
-            .validationFunctions
-            .reduce(
+            ?.validationFunctions
+            ?.reduce(
               (acc, nextFunc) => acc * nextFunc(fieldElement.value), true)) {
-            errorElement.textContent = fieldOptions.errorMessage;
+            errorElement.textContent =
+              fieldOptions?.errorMessage || this.#defaultInvalidError;
             willContinueSubmit = false;
           } else {
             errorElement.textContent = '';
           }
         }
-        if (willContinueSubmit) {
-          alert('The form is submitted successfully!');
+        for (const radioWrapperElement of
+          formElement.querySelectorAll('fieldset > div')) {
+          const radioTitleElement = radioWrapperElement.querySelector(
+            'span:first-of-type');
+          const radioOptions = this.#options.fields.find(
+            ({name}) => radioTitleElement.childNodes.item(
+                0).textContent ===
+              name.toTitleCase());
+          const radioErrorElement = radioWrapperElement.querySelector(
+            'div + span');
+          if (radioOptions?.isRequired && !Array.prototype.reduce.call(
+            radioWrapperElement.querySelectorAll('input'),
+            (acc, nextRadio) => acc + nextRadio.checked, false,
+          )) {
+            radioErrorElement.textContent =
+              radioOptions?.errorMessage || this.#defaultRequiredError;
+            willContinueSubmit = false;
+          } else {
+            radioErrorElement.textContent = '';
+          }
         }
+        if (!willContinueSubmit) {
+          return;
+        }
+        alert('The form is submitted successfully!');
       });
   }
 }
