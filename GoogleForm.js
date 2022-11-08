be290e2efcb327e0b3b507bb84cbbe0e9415fdc1
bernizhel <detrop.1995@gmail.com>
2022-11-08 17:20:35 +0300

    refactor: make the code more readable
    Add more spaces and logical breaks.
    Use seperate varialbes to contain field keywords.
    Change the invalid options error message.
    Change parameter names in addKeypressHandler, checkInputValidity and
    addSubmitHandler methods.

--- GoogleForm.js
+++ GoogleForm.js
@@ -1,23 +1,30 @@
 class GoogleForm {
   #options = {};
-  #formElement = this.#createElement('form', {noValidate: true});
+  #formElement = this.#createElement('form', { noValidate: true });
   #submitButtonElement = this.#createElement('button', {
     type: 'submit',
     innerText: 'Submit',
   });
+
   #inputElements = [];
   #radioElements = [];
   #checkboxElements = [];
   #selectElements = [];
+
   #defaultInvalidError = 'Please, input correct data.';
   #defaultRequiredError = 'Please, enter this field.';
   #defaultSelectOption = 'Not selected';
 
+  #inputKeyword = 'input';
+  #radioKeyword = 'radio';
+  #checkboxKeyword = 'checkbox';
+  #selectKeyword = 'select';
+
   constructor(options) {
     if (!this.#checkOptions(options)) {
-      throw new TypeError('The options argument passed by creating a new' +
-        ' GoogleForm instance must be of the corresponding type, please,' +
-        ' read the documentation on this class.');
+      throw new TypeError(
+        `The options argument is not valid. Read the documentation on this class.`
+      );
     }
     this.#options = options;
     this.#initializeForm(options);
@@ -27,41 +34,48 @@ class GoogleForm {
     if (!options?.title) {
       return false;
     }
+
     for (const field of options?.fields ?? []) {
       if (!field?.name || !field?.type?.keyword) {
         return false;
       }
     }
+
     return !(
-      ['radio', 'select'].includes(options?.type?.keyword) &&
+      [this.#radioKeyword, this.#selectKeyword].includes(options?.type?.keyword) &&
       !options?.type?.values
     );
   }
 
   #initializeForm(options) {
     const fieldsetElement = this.#createElement('fieldset');
-    fieldsetElement.appendChild(
-      this.#createElement('legend', {innerText: options.title}));
+    fieldsetElement.appendChild(this.#createElement('legend', { innerText: options.title }));
+
     if (options.description) {
-      fieldsetElement.appendChild(
-        this.#createElement('p', {innerText: options.description}));
+      fieldsetElement.appendChild(this.#createElement('p', { innerText: options.description }));
     }
+
     for (const fieldOptions of options.fields) {
-      if (fieldOptions.type.keyword === 'radio') {
-        fieldsetElement.appendChild(this.#createRadioField(fieldOptions));
-      }
-      if (fieldOptions.type.keyword === 'checkbox') {
-        fieldsetElement.appendChild(this.#createCheckboxField(fieldOptions));
-      }
-      if (fieldOptions.type.keyword === 'select') {
-        fieldsetElement.appendChild(this.#createSelectField(fieldOptions));
-      }
-      if (fieldOptions.type.keyword === 'input') {
+      if (fieldOptions.type.keyword === this.#inputKeyword) {
         fieldsetElement.appendChild(this.#createInputField(fieldOptions));
       }
+
+      if (fieldOptions.type.keyword === this.#radioKeyword) {
+        fieldsetElement.appendChild(this.#createRadioField(fieldOptions));
+      }
+
+      if (fieldOptions.type.keyword === this.#checkboxKeyword) {
+        fieldsetElement.appendChild(this.#createCheckboxField(fieldOptions));
+      }
+
+      if (fieldOptions.type.keyword === this.#selectKeyword) {
+        fieldsetElement.appendChild(this.#createSelectField(fieldOptions));
+      }
     }
+
     fieldsetElement.appendChild(this.#createElement('br'));
     fieldsetElement.appendChild(this.#submitButtonElement);
+
     this.#formElement.appendChild(fieldsetElement);
     this.#addSubmitHandler(this.#formElement);
   }
@@ -71,16 +85,20 @@ class GoogleForm {
   }
 
   #createInputField(fieldOptions) {
-    const labelElement = this.#createElement(
-      'label', {innerText: fieldOptions.title});
-    labelElement.appendChild(this.#createElement('span', {
-      innerText: fieldOptions.isRequired ? '*' : '',
-    }));
+    const labelElement = this.#createElement('label', {
+      innerText: fieldOptions.title,
+    });
+    labelElement.appendChild(
+      this.#createElement('span', {
+        innerText: fieldOptions.isRequired ? '*' : '',
+      })
+    );
     labelElement.appendChild(this.#createElement('br'));
-    const inputElement = this.#createElement(
-      'input', fieldOptions.attributes ?? {});
+
+    const inputElement = this.#createElement('input', fieldOptions.attributes ?? {});
     this.#addKeypressHandler(inputElement, fieldOptions);
     labelElement.appendChild(inputElement);
+
     const errorElement = this.#createElement('span');
     this.#inputElements.push([fieldOptions, inputElement, errorElement]);
     labelElement.appendChild(this.#createElement('br'));
@@ -94,20 +112,21 @@ class GoogleForm {
     const titleElement = this.#createElement('span', {
       innerText: fieldOptions.title,
     });
-    titleElement.appendChild(this.#createElement('span', {
-      innerText: fieldOptions.isRequired ? '*' : '',
-    }));
+    titleElement.appendChild(
+      this.#createElement('span', {
+        innerText: fieldOptions.isRequired ? '*' : '',
+      })
+    );
     wrapperElement.appendChild(titleElement);
     wrapperElement.appendChild(this.#createElement('br'));
+
     const innerWrapperElement = this.#createElement('div');
     const errorElement = this.#createElement('span');
     this.#radioElements.push([fieldOptions, [], errorElement]);
     for (const value of fieldOptions.type.values) {
       const labelElement = this.#createElement('label');
       const radioElement = this.#createElement('input', {
-        ...(
-          fieldOptions.attributes ?? {}
-        ),
+        ...(fieldOptions.attributes ?? {}),
         type: 'radio',
         name: fieldOptions.name,
         value,
@@ -119,6 +138,7 @@ class GoogleForm {
       innerWrapperElement.appendChild(labelElement);
       innerWrapperElement.appendChild(this.#createElement('br'));
     }
+
     wrapperElement.appendChild(innerWrapperElement);
     wrapperElement.appendChild(errorElement);
     wrapperElement.appendChild(this.#createElement('br'));
@@ -127,18 +147,21 @@ class GoogleForm {
 
   #createCheckboxField(fieldOptions) {
     const labelElement = this.#createElement('label');
+
     const checkboxElement = this.#createElement('input', {
-      ...(
-        fieldOptions.attributes ?? {}
-      ),
+      ...(fieldOptions.attributes ?? {}),
       type: 'checkbox',
     });
     this.#addKeypressHandler(checkboxElement, fieldOptions);
+
     labelElement.appendChild(checkboxElement);
     labelElement.appendChild(document.createTextNode(fieldOptions.title));
-    labelElement.appendChild(this.#createElement('span', {
-      innerText: fieldOptions.isRequired ? '*' : '',
-    }));
+    labelElement.appendChild(
+      this.#createElement('span', {
+        innerText: fieldOptions.isRequired ? '*' : '',
+      })
+    );
+
     const errorElement = this.#createElement('span');
     this.#checkboxElements.push([fieldOptions, checkboxElement]);
     labelElement.appendChild(this.#createElement('br'));
@@ -148,43 +171,53 @@ class GoogleForm {
   }
 
   #createSelectField(fieldOptions) {
-    const labelElement = this.#createElement(
-      'label', {innerText: fieldOptions.title});
-    labelElement.appendChild(this.#createElement('span', {
-      innerText: fieldOptions.isRequired ? '*' : '',
-    }));
+    const labelElement = this.#createElement('label', {
+      innerText: fieldOptions.title,
+    });
+    labelElement.appendChild(
+      this.#createElement('span', {
+        innerText: fieldOptions.isRequired ? '*' : '',
+      })
+    );
     labelElement.appendChild(this.#createElement('br'));
-    const selectElement = this.#createElement(
-      'select', fieldOptions.attributes ?? {});
-    selectElement.appendChild(this.#createElement('option', {
-      value: '',
-      selected: true,
-      disabled: !!fieldOptions.isRequired,
-      hidden: !!fieldOptions.isRequired,
-      innerText: this.#defaultSelectOption,
-    }));
+
+    const selectElement = this.#createElement('select', fieldOptions.attributes ?? {});
+    selectElement.appendChild(
+      this.#createElement('option', {
+        value: '',
+        selected: true,
+        disabled: !!fieldOptions.isRequired,
+        hidden: !!fieldOptions.isRequired,
+        innerText: this.#defaultSelectOption,
+      })
+    );
+
     if (Array.isArray(fieldOptions.type.values)) {
       for (const value of fieldOptions.type.values) {
-        selectElement.appendChild(this.#createElement('option', {
-          value,
-          innerText: value,
-        }));
+        selectElement.appendChild(
+          this.#createElement('option', {
+            value,
+            innerText: value,
+          })
+        );
       }
     } else {
-      for (const [valuesGroup, values] of
-        Object.entries(fieldOptions.type.values)) {
+      for (const [valuesGroup, values] of Object.entries(fieldOptions.type.values)) {
         const optgroupElement = this.#createElement('optgroup', {
           label: valuesGroup,
         });
         for (const value of values) {
-          optgroupElement.appendChild(this.#createElement('option', {
-            value,
-            innerText: value,
-          }));
+          optgroupElement.appendChild(
+            this.#createElement('option', {
+              value,
+              innerText: value,
+            })
+          );
         }
         selectElement.appendChild(optgroupElement);
       }
     }
+
     labelElement.appendChild(selectElement);
     const errorElement = this.#createElement('span');
     this.#selectElements.push([fieldOptions, selectElement, errorElement]);
@@ -194,13 +227,13 @@ class GoogleForm {
     return labelElement;
   }
 
-  #addKeypressHandler(fieldElement, fieldOptions) {
-    fieldElement.addEventListener('keypress', (event) => {
+  #addKeypressHandler(element, options) {
+    element.addEventListener('keypress', (event) => {
       event.stopImmediatePropagation();
       if (event.code === 'Enter') {
         event.preventDefault();
-        if (['radio', 'checkbox'].includes(fieldOptions.type.keyword)) {
-          fieldElement.checked = !fieldElement.checked;
+        if ([this.#radioKeyword, this.#checkboxKeyword].includes(options.type.keyword)) {
+          element.checked = !element.checked;
         } else {
           this.#submitButtonElement.focus();
         }
@@ -208,86 +241,93 @@ class GoogleForm {
     });
   }
 
-  #checkInputValidity(inputElement, fieldOptions) {
-    if (inputElement.value === '') {
+  #checkInputValidity(element, options) {
+    if (element.value === '') {
       return !fieldOptions.isRequired;
     }
-    if (!fieldOptions.validationFunctions) {
-      return !inputElement.validity.typeMismatch;
+
+    if (!options.validationFunctions) {
+      return !element.validity.typeMismatch;
     }
-    for (const validationFunction of fieldOptions.validationFunctions) {
-      if (!validationFunction(inputElement.value)) {
+
+    for (const validationFunction of options.validationFunctions) {
+      if (!validationFunction(element.value)) {
         return false;
       }
     }
+
     return true;
   }
 
-  #addSubmitHandler(formElement) {
-    formElement
-      .addEventListener('submit', (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        const submitData = {};
-        let willContinueSubmit = true;
-        let elementToFocus = null;
-        for (const [fieldOptions, inputElement, errorElement] of
-          this.#inputElements) {
-          if (!this.#checkInputValidity(inputElement, fieldOptions)) {
-            errorElement.textContent =
-              fieldOptions?.errorMessage ?? this.#defaultInvalidError;
-            if (!elementToFocus) {
-              elementToFocus = inputElement;
-            }
-            willContinueSubmit = false;
-          } else {
-            errorElement.textContent = '';
-            submitData[fieldOptions.name] = inputElement.value;
+  #addSubmitHandler(element) {
+    element.addEventListener('submit', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+
+      const submitData = {};
+      let willContinueSubmit = true;
+      let elementToFocus = null;
+      for (const [fieldOptions, inputElement, errorElement] of this.#inputElements) {
+        if (!this.#checkInputValidity(inputElement, fieldOptions)) {
+          errorElement.textContent = fieldOptions?.errorMessage ?? this.#defaultInvalidError;
+
+          if (!elementToFocus) {
+            elementToFocus = inputElement;
           }
+
+          willContinueSubmit = false;
+        } else {
+          errorElement.textContent = '';
+          submitData[fieldOptions.name] = inputElement.value;
         }
-        for (const [fieldOptions, radioElements, errorElement] of
-          this.#radioElements) {
-          if (fieldOptions?.isRequired &&
-            !radioElements.find(radio => radio.checked)) {
-            errorElement.textContent =
-              fieldOptions?.errorMessage ?? this.#defaultRequiredError;
-            if (!elementToFocus) {
-              elementToFocus = radioElements[0];
-            }
-            willContinueSubmit = false;
-          } else {
-            errorElement.textContent = '';
-            submitData[fieldOptions.name] =
-              radioElements.find((radioElement) => radioElement.checked).value;
+      }
+
+      for (const [fieldOptions, radioElements, errorElement] of this.#radioElements) {
+        if (fieldOptions?.isRequired && !radioElements.find((radio) => radio.checked)) {
+          errorElement.textContent = fieldOptions?.errorMessage ?? this.#defaultRequiredError;
+
+          if (!elementToFocus) {
+            elementToFocus = radioElements[0];
           }
+
+          willContinueSubmit = false;
+        } else {
+          errorElement.textContent = '';
+          submitData[fieldOptions.name] = radioElements.find(
+            (radioElement) => radioElement.checked
+          ).value;
         }
-        for (const [fieldOptions, checkboxElement] of this.#checkboxElements) {
-          submitData[fieldOptions.name] = checkboxElement.checked;
-        }
-        for (const [fieldOptions, selectElement, errorElement] of
-          this.#selectElements) {
-          if (fieldOptions?.isRequired && selectElement.value === '') {
-            errorElement.textContent =
-              fieldOptions?.errorMessage ?? this.#defaultRequiredError;
-            if (!elementToFocus) {
-              elementToFocus = selectElement;
-            }
-            willContinueSubmit = false;
-          } else {
-            errorElement.textContent = '';
-            submitData[fieldOptions.name] = selectElement.value;
+      }
+
+      for (const [fieldOptions, checkboxElement] of this.#checkboxElements) {
+        submitData[fieldOptions.name] = checkboxElement.checked;
+      }
+
+      for (const [fieldOptions, selectElement, errorElement] of this.#selectElements) {
+        if (fieldOptions?.isRequired && selectElement.value === '') {
+          errorElement.textContent = fieldOptions?.errorMessage ?? this.#defaultRequiredError;
+
+          if (!elementToFocus) {
+            elementToFocus = selectElement;
           }
+
+          willContinueSubmit = false;
+        } else {
+          errorElement.textContent = '';
+          submitData[fieldOptions.name] = selectElement.value;
         }
-        if (!willContinueSubmit) {
-          elementToFocus.focus();
-          return;
-        }
-        event.data = submitData;
-      });
+      }
+
+      if (!willContinueSubmit) {
+        elementToFocus.focus();
+        return;
+      }
+
+      event.data = submitData;
+    });
   }
 
   render(selector) {
-    document.querySelector(selector)
-      .appendChild(new GoogleForm(this.#options).#formElement);
+    document.querySelector(selector).appendChild(new GoogleForm(this.#options).#formElement);
   }
 }
--- index.js
+++ index.js
@@ -1,95 +1,107 @@
 const sampleForm = new GoogleForm({
   title: 'Your info',
   description: 'Please enter your info',
-  fields: [{
-    title: 'Name',
-    name: 'name',
-    isRequired: true,
-    validationFunctions: [isNameValid],
-    errorMessage: 'The name is invalid',
-    attributes: {},
-    type: {keyword: 'input'},
-  }, {
-    title: 'Age',
-    name: 'age',
-    isRequired: false,
-    validationFunctions: [isAgeValid],
-    errorMessage: 'The age is invalid',
-    attributes: {type: 'number'},
-    type: {keyword: 'input'},
-  }, {
-    title: 'Email',
-    name: 'email',
-    validationFunctions: [isEmailValid],
-    errorMessage: 'The email is invalid',
-    attributes: {type: 'email'},
-    type: {keyword: 'input'},
-  }, {
-    title: 'Password',
-    name: 'password',
-    attributes: {type: 'password'},
-    type: {keyword: 'input'},
-  }, {
-    title: 'Phone',
-    name: 'phone',
-    attributes: {type: 'tel'},
-    type: {keyword: 'input'},
-  }, {
-    title: 'URL',
-    name: 'url',
-    attributes: {type: 'url'},
-    type: {keyword: 'input'},
-  }, {
-    title: 'What color do you like?',
-    name: 'color',
-    isRequired: true,
-    validationFunctions: [],
-    attributes: {},
-    type: {
-      keyword: 'radio',
-      values: ['Red', 'Green', 'Blue'],
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      isRequired: true,
+      validationFunctions: [isNameValid],
+      errorMessage: 'The name is invalid',
+      attributes: {},
+      type: { keyword: 'input' },
     },
-  }, {
-    title: 'Do you have a dog?',
-    name: 'hasDog',
-    isRequired: true,
-    type: {keyword: 'checkbox'},
-  }, {
-    title: 'Do you have a cat?',
-    name: 'hasCat',
-    type: {keyword: 'checkbox'},
-  }, {
-    title: 'What country are you from?',
-    name: 'country',
-    isRequired: true,
-    type: {
-      keyword: 'select',
-      values: ['Russia', 'USA', 'China', 'France', 'Uzbekistan'],
+    {
+      title: 'Age',
+      name: 'age',
+      isRequired: false,
+      validationFunctions: [isAgeValid],
+      errorMessage: 'The age is invalid',
+      attributes: { type: 'number' },
+      type: { keyword: 'input' },
     },
-  }, {
-    title: 'What city are you from?',
-    name: 'city',
-    type: {
-      keyword: 'select',
-      values: {
-        'Russia': ['Moscow', 'Saint-Petersburg', 'Voronezh', 'Omsk'],
-        'USA': ['New York', 'Washington', 'Detroit', 'Miami'],
-        'China': ['Beijing', 'Hong-Kong', 'Shanghai'],
-        'France': ['Paris'],
-        'Uzbekistan': ['Tashkent'],
+    {
+      title: 'Email',
+      name: 'email',
+      validationFunctions: [isEmailValid],
+      errorMessage: 'The email is invalid',
+      attributes: { type: 'email' },
+      type: { keyword: 'input' },
+    },
+    {
+      title: 'Password',
+      name: 'password',
+      attributes: { type: 'password' },
+      type: { keyword: 'input' },
+    },
+    {
+      title: 'Phone',
+      name: 'phone',
+      attributes: { type: 'tel' },
+      type: { keyword: 'input' },
+    },
+    {
+      title: 'URL',
+      name: 'url',
+      attributes: { type: 'url' },
+      type: { keyword: 'input' },
+    },
+    {
+      title: 'What color do you like?',
+      name: 'color',
+      isRequired: true,
+      validationFunctions: [],
+      attributes: {},
+      type: {
+        keyword: 'radio',
+        values: ['Red', 'Green', 'Blue'],
       },
     },
-  }],
+    {
+      title: 'Do you have a dog?',
+      name: 'hasDog',
+      isRequired: true,
+      type: { keyword: 'checkbox' },
+    },
+    {
+      title: 'Do you have a cat?',
+      name: 'hasCat',
+      type: { keyword: 'checkbox' },
+    },
+    {
+      title: 'What country are you from?',
+      name: 'country',
+      isRequired: true,
+      type: {
+        keyword: 'select',
+        values: ['Russia', 'USA', 'China', 'France', 'Uzbekistan'],
+      },
+    },
+    {
+      title: 'What city are you from?',
+      name: 'city',
+      type: {
+        keyword: 'select',
+        values: {
+          Russia: ['Moscow', 'Saint-Petersburg', 'Voronezh', 'Omsk'],
+          USA: ['New York', 'Washington', 'Detroit', 'Miami'],
+          China: ['Beijing', 'Hong-Kong', 'Shanghai'],
+          France: ['Paris'],
+          Uzbekistan: ['Tashkent'],
+        },
+      },
+    },
+  ],
 });
 
 sampleForm.render('.google-form');
 sampleForm.render('.google-form');
 
-document.querySelectorAll('.google-form form')
-  .forEach(form => form
-    .addEventListener('submit', (event) => {
-      if (!event.data) {
-        return;
-      }
-      console.table(event.data);
-    }));
+document.querySelectorAll('.google-form form').forEach((form) =>
+  form.addEventListener('submit', (event) => {
+    if (!event.data) {
+      return;
+    }
+    console.table(event.data);
+  })
+);
